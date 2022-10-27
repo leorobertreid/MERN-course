5c44901a1a43eca76a28808e0dfff437110d2f87
Leo Reid <leo.reid.mail@gmail.com>
2022-10-27 10:22:30 +0200

    created a simple frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,35 @@
 import React from 'react'
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core'
+
+import Posts from './components/Posts/Posts.js'
+import Form from './components/Form/Form.js'
+import memories from './images/memories.png'
+import useStyles from './styles.js'
 
 const App = () => {
+    const classes = useStyles()
+
     return (
-        <div>
-            <h1>App</h1>
-        </div>
+        <Container maxwidth="lg">
+            <AppBar className={classes.appBar} position="static" color="inherit">
+                <Typography className={classes.heading} variant="h2" align="center">
+                    Memories
+                </Typography>
+                <img className={classes.image} src={memories} alt="memories" height="60"></img>
+            </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid container justifyContent='space-between' alignItems='stretch' spacing>
+                        <Grid item xs={12} sm={7}>
+                            <Posts></Posts>
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <Form></Form>
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Grow>
+        </Container>
     )
 }
 
